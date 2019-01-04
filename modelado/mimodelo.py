61d54597ec3940dae7d4174988e9108ade1df098
Josue Rojas <jsebastianrojas19@gmail.com>
2019-01-04 15:05:23 -0500

    subiendo trabajo completo

--- modelado/mimodelo.py
+++ modelado/mimodelo.py
@@ -1,14 +1,15 @@
 import codecs
 
-
+#clase Equipo
 class Equipo (object):
-
+#constructor que recibe atriutos tipo String e int
     def __init__(self, nom, ciu, camp, numj):
         self.nombre = nom
         self.ciudad = ciu
         self.campeonatos = int(camp)
         self.numjugadores = int(numj)
 
+#metodos agregar y obtener de los atributos
     def agregar_nombre(self, nom):
         """
         """
@@ -49,6 +50,7 @@ class Equipo (object):
         """
         return self.numjugadores
 
+#sSe sobreescribe el metodo str
     def __str__(self):
         """
         """
@@ -59,28 +61,28 @@ class Equipo (object):
         """
         return "%s %s %d %d" % (self.nombre, self.ciudad, self.campeonatos, self.numjugadores)
 
-
+#clase Operaciones
 class Operaciones(object):
+    #constructor de la clase
     def __init__(self, listado):
         self.listado_equipo = listado
 
-    def ordenar(self):
+#metodo ordear, para ordenar la lista de acuerdo al usuario
+    def ordenar(self, p):
         """
         """
+        if p == "nombre":
+            return sorted(self.listado_equipo, key=lambda equipo: equipo.nombre)
+        else:
+            if p == "campeonato":
+                return sorted(self.listado_equipo, key=lambda equipo: equipo.campeonatos)
 
-        return sorted(self.listado_equipo, key=lambda equipo: equipo.nombre)
-    def ordenar2(self):
-        """
-        """
-
-        return sorted(self.listado_equipo, key=lambda equipo: equipo.campeonatos)
-
-
+#clase MiArcchivp
 class MiArchivo:
 
     def __init__(self):
-        self.archivo = codecs.open("informacion.csv", "r")
-
+        self.archivo = codecs.open("informacion.csv", "r")#abre el rchivo
+#metodo para la informacion que se encuentra en el archivo
     def obtener_informacion(self):
         return self.archivo.readlines()
 
@@ -91,11 +93,11 @@ class MiArchivo:
 class MiArchivoEscribir:
 
     def __init__(self):
-        self.archivo = codecs.open("ordenamiento.csv", "a")
+        self.archivo = codecs.open("ordenamiento.csv", "a") #crea el archivo
 
-
-    def setInformacion(self, p):
-        self.archivo.write()
+#metodo para agregar la informacion al archivo
+    def agregar_informacion(self, c):
+        self.archivo.write("%s %s %d %d" % (c.nombre, c.ciudad, c.campeonatos, c.numjugadores))
 
     def cerrar_archivo(self):
         self.archivo.close()
--- principal.py
+++ principal.py
@@ -1,17 +1,28 @@
-from modelado.mimodelo import Equipo, Operaciones ,MiArchivo, MiArchivoEscribir
-
+from modelado.mimodelo import Equipo, Operaciones ,MiArchivo, MiArchivoEscribir #importacion de las clases en el archivo mimodelo
+#declaracion de objetos
 m = MiArchivo()
+c = MiArchivoEscribir()
 lista = m.obtener_informacion()
 lista = [l.split("|") for l in lista]
 lista_equipo = []
-
+#for para recorrer la lista
 for d in lista:
-    p = Equipo(d[0], d[1], d[2], d[3])
-    lista_equipo.append(p)
-
+    e = Equipo(d[0], d[1], d[2], d[3])
+    lista_equipo.append(e)
+#creacion de objeto tipo Operaciones
 operacion = Operaciones(lista_equipo)
-lista_ordenada = operacion.ordenar()
-print(lista_ordenada)
-lista_ordenada2 = operacion.ordenar2()
-print(lista_ordenada2)
+opc = input("Desea ordenar por nombre o campeonato: ")
+lista_ordenada = operacion.ordenar(opc)
+
+#for que recorre la lista ordenada
+for g in lista_ordenada:
+    c.agregar_informacion(g)#manda la informacion del objeto al metodo agregar informacion
+    print(g)
+
+m.cerrar_archivo()
+c.cerrar_archivo()
+
+
+
+
 
